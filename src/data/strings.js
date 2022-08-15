0e879d4151aea06c1e6821faaa06151de955ddfc
Hikmatyar Mujahidin <hikmatyar@outlook.co.id>
2022-08-15 18:39:39 +0700

    modify strings

--- src/data/strings.js
+++ src/data/strings.js
@@ -1,5 +1,14 @@
-export const LANDING_TITLE = 'Getting your every task to new heights.';
+export const LANDING_TITLE = 'Get tasks done. Quality, fast, now.';
 export const LANDING_DESC =
-  'launch.api completes tasks essential for your business to thrive.';
-export const LANDING_BTN_TEXT = 'Post a job now';
+  'Launch sources suitable talent to complete your tasks, connecting you with the newest and brightest talent.';
+export const LANDING_BTN_TEXT_POST = 'Post a job now';
+export const LANDING_BTN_TEXT_FIND = 'Find Talent';
 export const LOGIN_BG_TEXT = "Can't wait for you to be a part of our community";
+export const LANDING_TITLE_BRIEF =
+  'Outsource time sensitive tasks to quality freelancers';
+export const LANDING_TEXT_BRIEF =
+  'Businesses spend countless time, capital and human resources to find new talent for tasks that can be easily outsourced for cheaper, with full end to end control over its delivery.';
+export const LANDING_SUB_TEXT_BRIEF = 'Have your needs met. Today.';
+export const LANDING_TEXT_DESC = "Here's how it works";
+export const LANDING_TEXT_TALENT =
+  "Here's some of our talent and the work they've produced";
