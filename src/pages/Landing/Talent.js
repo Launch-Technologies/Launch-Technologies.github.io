4dbd00130e25b4c7d9131c01370fa707d63b14fe
Hikmatyar Mujahidin <hikmatyar@outlook.co.id>
2022-08-23 10:25:39 +0700

    import scoped styling to child component as well

--- src/pages/Landing/Talent.js
+++ src/pages/Landing/Talent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Col } from 'antd';
+import './Landing.scoped.css';
 
 const Talent = ({ name, job, desc, img_path }) => {
   return (
@@ -10,6 +11,7 @@ const Talent = ({ name, job, desc, img_path }) => {
         cover={
           <img
             className="img_card_talent"
+            alt={name}
             // src="https://s3-alpha-sig.figma.com/img/dbde/eeac/16cc041a6670ebd271437f44536034a7?Expires=1661731200&Signature=fN7lA1nDxxPs9mJE25b0~D72C34V1EwIGYP80zeUuk56sPgMRB52dNTfbiwURghpIVdYoIlV0uc8XSk4b1C7johc8Y1y2uX1COQ7wEb0wTOqCIKvPPFFGqZg7CnDW61JB6U99kjvgy-9IrRFbmwre4NVsCXCqsgnLzO3DKjeRNtpbbEuPIikmyKkEyanpinKf14IRjUdKpdiw0Jij0XAfyT3JEgTUvQ5yKqDXnx4fFm09CYZJvqygx4xMFTXpQWsYwgNOBPMdG2pjksQOUA~i94pUeVLudY5v8za7EUJeSfK64knmdx6uvvfXaEr6vsb7gNisUyFAmKl7alYmFgEkg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
             src={img_path}
           />
